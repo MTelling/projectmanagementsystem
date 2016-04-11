175f2a2edb5c940cd97de16c8d4c9fa5dbba3259
Morten Telling <mtelling1995@gmail.com>
2016-04-11 13:14:55 +0200

    Refactored test

--- tests/dk/dtu/software/group8/TestCreateProject.java
+++ tests/dk/dtu/software/group8/TestCreateProject.java
@@ -1,7 +1,9 @@
 package dk.dtu.software.group8;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.Calendar;
 
@@ -9,6 +11,9 @@ public class TestCreateProject {
 	PManagementSystem pms;
 	DatabaseManager db;
 	DateServer dateServer;
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
 	@Before
 	public void setup() {
@@ -26,9 +31,8 @@ public class TestCreateProject {
 	}
 
 
-	@Test
+	@Test //Correct start and end.
 	public void createProjectA() {
-        //Make correct start date and end date.
         Calendar startDate = dateServer.getDate();
         startDate.set(2016, Calendar.MAY, 10);
 
@@ -46,17 +50,101 @@ public class TestCreateProject {
         assertEquals(project.getEndDate(), endDate);
 	}
 
-    @Test
-    public void createProjectC() {
-        //Make correct start date and end date.
+
+    @Test //Correct start and end.
+    public void createProjectB() {
         Calendar startDate = dateServer.getDate();
         startDate.set(2016, Calendar.MAY, 10);
 
         Calendar endDate = dateServer.getDate();
-        endDate.set(2016, Calendar.JUNE, 10);
+        endDate.set(2016, Calendar.MAY, 10);
 
         //Create the project
         Project project = pms.createProject(startDate, endDate);
+
+        //Check that the project was added.
+        assertEquals(pms.getProjects().size(), 1);
+
+        //Test that date is set correct.
+        assertEquals(project.getStartDate(), startDate);
+        assertEquals(project.getEndDate(), endDate);
+    }
+
+    @Test //End date before start.
+    public void createProjectC() {
+        Calendar startDate = dateServer.getDate();
+        startDate.set(2016, Calendar.MAY, 10);
+
+        Calendar endDate = dateServer.getDate();
+        endDate.set(2016, Calendar.MAY, 3);
+
+        //Create the project
+        Project project = pms.createProject(startDate, endDate);
+
+        //Check correct exception
+        expectedEx.expect(WrongDateException.class);
+        expectedEx.expectMessage("End date is before start date");
+
+        //Check that the project was null.
+        assertNull(project);
+
+        //Check that the project wasn't added.
+        assertEquals(pms.getProjects().size(), 0);
+    }
+
+    @Test //Start date in the past, correct end date
+    public void createProjectD() {
+        Calendar startDate = dateServer.getDate();
+        startDate.set(2016, Calendar.APRIL, 25);
+
+        Calendar endDate = dateServer.getDate();
+        endDate.set(2016, Calendar.MAY, 10);
+
+        //Create the project
+        Project project = pms.createProject(startDate, endDate);
+
+        //Check correct exception
+        expectedEx.expect(WrongDateException.class);
+        expectedEx.expectMessage("Start date is in the past");
+
+        //Check that the project was null.
+        assertNull(project);
+
+        //Check that the project wasn't added.
+        assertEquals(pms.getProjects().size(), 0);
+    }
+
+    @Test //Incorrect start date, incorrect end date. (Both in past)
+    public void createProjectE() {
+        Calendar startDate = dateServer.getDate();
+        startDate.set(2016, Calendar.APRIL, 25);
+
+        Calendar endDate = dateServer.getDate();
+        endDate.set(2016, Calendar.APRIL, 30);
+
+        //Create the project
+        Project project = pms.createProject(startDate, endDate);
+
+        //Check correct exception
+        expectedEx.expect(WrongDateException.class);
+        expectedEx.expectMessage("Both start and end date is in the past.");
+
+        //Check that the project was null.
+        assertNull(project);
+
+        //Check that the project wasn't added.
+        assertEquals(pms.getProjects().size(), 0);
+    }
+
+    @Test //No dates.
+    public void createProjectF() {
+
+        //Create the project with no dates.
+        Project project = pms.createProject(null, null);
+
+        //Check correct exception
+        expectedEx.expect(WrongDateException.class);
+        expectedEx.expectMessage("No dates were given.");
 
         //Check that the project was null.
         assertNull(project);
@@ -67,15 +155,7 @@ public class TestCreateProject {
 
 
 
-
     //These are not described in our test:
-//	@Test
-//	public void createProjectNotSignedIn() {
-//		assertEquals(pms.getProjects().size(), 0);
-//		pms.createProject();
-//		assertEquals(pms.getProjects().size(), 0);
-//	}
-//
 //	@Test
 //	public void testForActualProject() {
 //		pms.signIn(db.getEmployees()[0]);
