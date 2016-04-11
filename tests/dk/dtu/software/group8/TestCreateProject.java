d8d0a2c00f1b1ddca5694968bda02201520bf77d
Morten Telling <mtelling1995@gmail.com>
2016-04-11 14:21:08 +0200

    Fixed calendars in TestCreateProject.

--- tests/dk/dtu/software/group8/TestCreateProject.java
+++ tests/dk/dtu/software/group8/TestCreateProject.java
@@ -40,14 +40,11 @@ public class TestCreateProject {
 	}
 
 
-
+//
 //	@Test //Correct start and end.
 //	public void createProjectA() {
-//        Calendar startDate = pms.getDate();
-//        startDate.set(2016, Calendar.MAY, 10);
-//
-//        Calendar endDate = pms.getDate();
-//        endDate.set(2016, Calendar.JUNE, 10);
+//        Calendar startDate = new GregorianCalendar(2016, Calendar.MAY, 10);
+//        Calendar endDate = new GregorianCalendar(2016, Calendar.JUNE, 10);
 //
 //        //Create the project
 //		Project project = pms.createProject(startDate, endDate);
@@ -59,15 +56,12 @@ public class TestCreateProject {
 //        assertEquals(project.getStartDate(), startDate);
 //        assertEquals(project.getEndDate(), endDate);
 //	}
-
-
+//
+//
 //    @Test //Correct start and end.
 //    public void createProjectB() {
-//        Calendar startDate = pms.getDate();
-//        startDate.set(2016, Calendar.MAY, 10);
-//
-//        Calendar endDate = pms.getDate();
-//        endDate.set(2016, Calendar.MAY, 10);
+//        Calendar startDate = new GregorianCalendar(2016, Calendar.MAY, 10);
+//        Calendar endDate = new GregorianCalendar(2016, Calendar.MAY, 10);
 //
 //        //Create the project
 //        Project project = pms.createProject(startDate, endDate);
@@ -79,14 +73,11 @@ public class TestCreateProject {
 //        assertEquals(project.getStartDate(), startDate);
 //        assertEquals(project.getEndDate(), endDate);
 //    }
-
+//
 //    @Test //End date before start.
 //    public void createProjectC() {
-//        Calendar startDate = pms.getDate();
-//        startDate.set(2016, Calendar.MAY, 10);
-//
-//        Calendar endDate = pms.getDate();
-//        endDate.set(2016, Calendar.MAY, 3);
+//        Calendar startDate = new GregorianCalendar(2016, Calendar.MAY, 10);
+//        Calendar endDate = new GregorianCalendar(2016, Calendar.MAY, 3);
 //
 //        //Create the project
 //        Project project = pms.createProject(startDate, endDate);
@@ -101,14 +92,11 @@ public class TestCreateProject {
 //        //Check that the project wasn't added.
 //        assertEquals(pms.getProjects().size(), 0);
 //    }
-
+//
 //    @Test //Start date in the past, correct end date
 //    public void createProjectD() {
-//        Calendar startDate = pms.getDate();
-//        startDate.set(2016, Calendar.APRIL, 25);
-//
-//        Calendar endDate = pms.getDate();
-//        endDate.set(2016, Calendar.MAY, 10);
+//        Calendar startDate = new GregorianCalendar(2016, Calendar.APRIL, 25);
+//        Calendar endDate = new GregorianCalendar(2016, Calendar.MAY, 10);
 //
 //        //Create the project
 //        Project project = pms.createProject(startDate, endDate);
@@ -123,21 +111,18 @@ public class TestCreateProject {
 //        //Check that the project wasn't added.
 //        assertEquals(pms.getProjects().size(), 0);
 //    }
-
+//
 //    @Test //Incorrect start date, incorrect end date. (Both in past)
 //    public void createProjectE() {
-//        Calendar startDate = pms.getDate();
-//        startDate.set(2016, Calendar.APRIL, 25);
-//
-//        Calendar endDate = pms.getDate();
-//        endDate.set(2016, Calendar.APRIL, 30);
+//        Calendar startDate = new GregorianCalendar(2016, Calendar.APRIL, 25);
+//        Calendar endDate = new GregorianCalendar(2016, Calendar.APRIL, 30);
 //
 //        //Create the project
 //        Project project = pms.createProject(startDate, endDate);
 //
 //        //Check correct exception
 //        expectedEx.expect(WrongDateException.class);
-//        expectedEx.expectMessage("Both start and end date is in the past.");
+//        expectedEx.expectMessage("Both start and end date are in the past.");
 //
 //        //Check that the project was null.
 //        assertNull(project);
@@ -162,8 +147,8 @@ public class TestCreateProject {
 //        //Check that the project wasn't added.
 //        assertEquals(pms.getProjects().size(), 0);
 //    }
-//
-//
+
+
 
     //TODO: These are not described in our test:
 //	@Test
