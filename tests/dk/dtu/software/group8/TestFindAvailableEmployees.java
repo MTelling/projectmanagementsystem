bd492885ee8df9f265ade222236b5b302fc2d078
Tobias Lindstrøm <Toli.TobiasLindstrom@gmail.com>
2016-05-03 15:31:16 +0200

    Wrote tests for findAvailableEmployees

--- tests/dk/dtu/software/group8/TestFindAvailableEmployees.java
+++ tests/dk/dtu/software/group8/TestFindAvailableEmployees.java
@@ -3,15 +3,20 @@ package dk.dtu.software.group8;
 
 import dk.dtu.software.group8.Exceptions.IncorrectAttributeException;
 import dk.dtu.software.group8.Exceptions.NoAccessException;
+import dk.dtu.software.group8.Exceptions.WrongDateException;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
 
 public class TestFindAvailableEmployees extends TestManageProject {
 
-    Activity activity;
+    ProjectActivity activity;
 
     @Before
     public void setupActivity() throws IncorrectAttributeException, NoAccessException {
@@ -19,9 +24,89 @@ public class TestFindAvailableEmployees extends TestManageProject {
         assertThat(activity, instanceOf(ProjectActivity.class));
     }
 
-    @Test
-    public void testFindAvailableEmployees() {
-
-    }
+//    @Test
+//    public void testFindAvailableEmployees() throws Exception {
+//        LocalDate startDate = LocalDate.parse("2016-05-10");
+//        LocalDate endDate = LocalDate.parse("2016-05-15");
+//
+//        List<Employee> availableEmployees = pms.findAvailableEmployees(startDate, endDate, activity);
+//        assertThat(availableEmployees, equalTo(db.getEmployees()));
+//    }
+//
+//    @Test
+//    public void testEmployeeWith20Activities() throws Exception {
+//        LocalDate startDate = LocalDate.parse("2016-05-10");
+//        LocalDate endDate = LocalDate.parse("2016-05-15");
+//
+//        List<Employee> availableEmployees = pms.findAvailableEmployees(startDate, endDate, activity);
+//
+//        Employee occupiedEmployee = db.getEmployees().get(2);
+//
+//        for(int i = 0; i < 20; i++) {
+//            ProjectActivity activityQ = pms.createActivityForProject(project, "Jon Jones", new YearWeek(2016, 1), new YearWeek(2016, 52), 1850);
+//            pms.addEmployeeToActivity(project, activityQ, occupiedEmployee);
+//        }
+//
+//        List<Employee> actualAvailableEmployees = db.getEmployees();
+//        actualAvailableEmployees.remove(occupiedEmployee);
+//
+//        assertThat(availableEmployees, equalTo(db.getEmployees()));
+//    }
+//
+//    @Test
+//    public void testEmployeeWithPersonalActivity() throws Exception {
+//        LocalDate startDate = LocalDate.parse("2016-05-10");
+//        LocalDate endDate = LocalDate.parse("2016-05-15");
+//
+//        List<Employee> availableEmployees = pms.findAvailableEmployees(startDate, endDate, activity);
+//
+//        Employee occupiedEmployee = db.getEmployees().get(2);
+//
+//        pms.createPersonalActivityForEmployee("Vacation", LocalDate.parse("2016-01-01"), LocalDate.parse("2016-12-31"), occupiedEmployee);
+//
+//        List<Employee> actualAvailableEmployees = db.getEmployees();
+//        actualAvailableEmployees.remove(occupiedEmployee);
+//
+//        assertThat(availableEmployees, equalTo(db.getEmployees()));
+//    }
+//
+//    @Test
+//    public void testEmployeeAlreadyInActivity() throws Exception {
+//        LocalDate startDate = LocalDate.parse("2016-05-10");
+//        LocalDate endDate = LocalDate.parse("2016-05-15");
+//
+//        List<Employee> availableEmployees = pms.findAvailableEmployees(startDate, endDate, activity);
+//
+//        Employee occupiedEmployee = db.getEmployees().get(2);
+//
+//        pms.addEmployeeToActivity(project, activity, occupiedEmployee);
+//
+//        List<Employee> actualAvailableEmployees = db.getEmployees();
+//        actualAvailableEmployees.remove(occupiedEmployee);
+//
+//        assertThat(availableEmployees, equalTo(db.getEmployees()));
+//    }
+//
+//    @Test
+//    public void testStartDateInThePast() throws Exception {
+//        expectedEx.expect(WrongDateException.class);
+//        expectedEx.expectMessage("Start date is not allowed to be in the past!");
+//
+//        LocalDate startDate = LocalDate.parse("2016-04-10");
+//        LocalDate endDate = LocalDate.parse("2016-05-15");
+//
+//        pms.findAvailableEmployees(startDate, endDate, activity);
+//    }
+//
+//    @Test
+//    public void testEndDateIsBeforeStartDate() throws Exception {
+//        expectedEx.expect(WrongDateException.class);
+//        expectedEx.expectMessage("End date is not allowed to be before start date!");
+//
+//        LocalDate startDate = LocalDate.parse("2016-05-15");
+//        LocalDate endDate = LocalDate.parse("2016-05-10");
+//
+//        pms.findAvailableEmployees(startDate, endDate, activity);
+//    }
 
 }
